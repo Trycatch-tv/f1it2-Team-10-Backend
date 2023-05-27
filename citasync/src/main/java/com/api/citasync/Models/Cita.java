30f497e3cc91cb6772620c4d9f263cc85d585bdc
Yf3rs0n <yferson5020@gmail.com>
2023-05-27 15:37:31 -0500

    construcción del modelo Cita con métodos y constructores, Yeferson Agudelo

--- citasync/src/main/java/com/api/citasync/Models/Cita.java
+++ citasync/src/main/java/com/api/citasync/Models/Cita.java
@@ -1,4 +1,44 @@
 package com.api.citasync.Models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Getter//Métodos Getters
+@Setter//Métodos Setters
+@NoArgsConstructor//constructor sin parametros
+@AllArgsConstructor//constructor con parametros
+@Entity
+@Table(name = "citas")
 public class Cita {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "fecha_cita")
+    private Date fecha;
+
+    @Column(name = "hora_cita")
+    private Time hora;
+
+    @Column(name = "duracion")
+    private int duracion;
+
+    @Column(name = "ubicacion")
+    private String ubicacion;
+
+    @Column(name = "detalles", columnDefinition = "TEXT")
+    private String detalles;
+
+    @Column(name = "estado")
+    private String estado;
+
 }
