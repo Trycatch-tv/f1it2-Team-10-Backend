3f249ac973f07c433ef53d4ea7381cc0adf04278
Yf3rs0n <yferson5020@gmail.com>
2023-05-27 19:08:20 -0500

    Se configuró las clases CitaRestController, CitaService y la interfaz ICitaRepository para la creación de dos métodos Http Crear y Listar Citas, Yeferson Agudelo

--- citasync/src/main/java/com/api/citasync/Repositories/ICitaRepository.java
+++ citasync/src/main/java/com/api/citasync/Repositories/ICitaRepository.java
@@ -1,4 +1,11 @@
 package com.api.citasync.Repositories;
 
-public interface ICitaRepository {
+import com.api.citasync.Models.Cita;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+// Esta interfaz proporciona métodos para acceder a citas en la base de datos.
+// Extiende JpaRepository<Cita, Long> para heredar métodos para operaciones CRUD básicas en citas.
+public interface ICitaRepository extends JpaRepository<Cita, Long> {
 }
