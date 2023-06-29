63e3cf9e6eafeeba261f9974c0531e585a4390b7
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2023-06-29 09:15:12 -0500

    :bug: (fix) Fix CORS credentials config.

--- citasync/src/main/java/com/api/citasync/config/WebConfig.java
+++ citasync/src/main/java/com/api/citasync/config/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
